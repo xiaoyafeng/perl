29637f8c7ab1668aa5ec243f5a94af3a3ccea025
Karl Williamson <public@khwilliamson.com>
2013-02-07 21:35:57 -0700

    Don't use EBCDIC macro for a C language escape
    C recognizes '\a' (for BEL); just use that instead of a look-up.
    
    regen/unicode_constants.pl could be used to generate the character for
    the ESC (set in surrounding code), but I didn't do that because of
    potential bootstrapping problems when porting to an EBCDIC platform
    without a working perl.  (The other characters generated in that .pl are
    less likely to cause problems when compiling perl.)

--- regcomp.c
+++ regcomp.c
@@ -10866,7 +10866,7 @@ tryagain:
 			p++;
 			break;
 		    case 'a':
-			  ender = ASCII_TO_NATIVE('\007');
+			  ender = '\a';
 			p++;
 			break;
 		    case 'o':
@@ -12466,7 +12466,7 @@ parseit:
 	    case 'f':	value = '\f';			break;
 	    case 'b':	value = '\b';			break;
 	    case 'e':	value = ASCII_TO_NATIVE('\033');break;
-	    case 'a':	value = ASCII_TO_NATIVE('\007');break;
+	    case 'a':	value = '\a';                   break;
 	    case 'o':
 		RExC_parse--;	/* function expects to be pointed at the 'o' */
 		{
