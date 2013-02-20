006f4c4108e199586ecfa76cb874d01da6704926
Karl Williamson <public@khwilliamson.com>
2013-02-20 10:26:43 -0700

    Remove remaining calls of NATIVE_TO_NEED
    These calls are just copying the input to the output byte by byte.
    There is no need to worry about UTF-8 or not, as the output is just an
    exact copy of the input

--- toke.c
+++ toke.c
@@ -3159,7 +3159,7 @@ S_scan_const(pTHX_ char *start)
 	else if (*s == '(' && PL_lex_inpat && s[1] == '?') {
 	    if (s[2] == '#') {
 		while (s+1 < send && *s != ')')
-		    *d++ = NATIVE_TO_NEED(has_utf8,*s++);
+		    *d++ = *s++;
 	    }
 	    else if (!PL_lex_casemods && !in_charclass &&
 		     (    s[2] == '{' /* This should match regcomp.c */
@@ -3173,7 +3173,7 @@ S_scan_const(pTHX_ char *start)
 	else if (*s == '#' && PL_lex_inpat &&
 	  ((PMOP*)PL_lex_inpat)->op_pmflags & RXf_PMf_EXTENDED) {
 	    while (s+1 < send && *s != '\n')
-		*d++ = NATIVE_TO_NEED(has_utf8,*s++);
+		*d++ = *s++;
 	}
 
 	/* no further processing of single-quoted regex */
@@ -3714,7 +3714,7 @@ S_scan_const(pTHX_ char *start)
 #endif
 	}
 	else {
-	    *d++ = NATIVE_TO_NEED(has_utf8,*s++);
+	    *d++ = *s++;
 	}
     } /* while loop to process each character */
 
