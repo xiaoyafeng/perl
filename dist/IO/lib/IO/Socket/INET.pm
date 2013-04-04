1578e7d037d6b874497b2eaa083869e9d96d2632
Matthew Horsfall (alh) <WolfSage@gmail.com>
2013-04-04 11:00:33 -0400

    Apply patch from Paul Evans

--- dist/IO/lib/IO/Socket/INET.pm
+++ dist/IO/lib/IO/Socket/INET.pm
@@ -1,5 +1,6 @@
 package IO::Socket::INET;
 
+use IO::Socket::IP -register;
 use base qw( IO::Socket::IP );
 use Socket qw( PF_INET );
 
@@ -7,7 +8,7 @@ sub new
 {
   my $class = shift;
   return $class->SUPER::new(Family => PF_INET, PeerAddr => shift) if @_ == 1;
-  return $class->SUPER::new(@_);
+  return $class->SUPER::new(Family => PF_INET, @_);
 }
 
 1;
